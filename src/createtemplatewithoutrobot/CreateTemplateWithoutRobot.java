c6e6378cebd6a879a02b64eb2d72fa784bc3d9b1
Luan Grabher <luanggcontato@gmail.com>
2021-08-31 15:39:37 -0300

    first commit

---
+++ src/createtemplatewithoutrobot/CreateTemplateWithoutRobot.java
@@ -0,0 +1,12 @@
+package createtemplatewithoutrobot;
+
+public class CreateTemplateWithoutRobot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
